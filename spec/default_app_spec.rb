d8b8590201d6550bd0a4d371aff86fd52250c345
Maher Hawash <gmhawash@gmail.com>
2013-05-01 08:45:54 -0700

    rename sample_app to fixture_app because it is not really a sample app after all

---
+++ spec/default_app_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require File.expand_path('../fixture_app/config/default_app', __FILE__)
+
+describe SampleApp do
+  describe "Bumbleworks.configure: use default folders" do
+    let(:app_root) {File.expand_path('../fixture_app', __FILE__)}
+
+    before :each do
+      Bumbleworks.reset!
+    end
+
+    it 'sets Bumbleworks configuration for application root' do
+      described_class.new
+      Bumbleworks.root.should == app_root
+    end
+
+    it 'discovers default Root/lib/process_definitions folder' do
+      described_class.new
+      Bumbleworks.definitions_directory.should == File.join(app_root, 'lib/process_definitions')
+    end
+
+    it 'discovers default Root/app/participants directory folder' do
+      described_class.new
+      Bumbleworks.participants_directory.should == File.join(app_root, 'app', 'participants' )
+    end
+
+    it 'discovers default Root/participants directory folder' do
+      described_class.new
+      Bumbleworks.root = File.join(app_root, 'app')
+      Bumbleworks.participants_directory.should == File.join(app_root, 'app', 'participants' )
+    end
+  end
+end
+
