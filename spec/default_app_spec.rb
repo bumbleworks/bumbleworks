af36be8d776609dd2ab6aabd077f8048152bb359
Maher Hawash <gmhawash@gmail.com>
2013-05-03 12:25:13 -0700

    Add default catchall participant in case the user forgot to do so.

--- spec/default_app_spec.rb
+++ spec/default_app_spec.rb
@@ -5,36 +5,49 @@ describe SampleApp do
   describe "Bumbleworks.configure: use default folders" do
     let(:app_root) {File.expand_path('../fixture_app', __FILE__)}
 
-    it 'sets Bumbleworks configuration for application root' do
+    before :each do
       described_class.new
+    end
+
+    it 'sets Bumbleworks configuration for application root' do
       Bumbleworks.root.should == app_root
     end
 
     it 'discovers default Root/lib/process_definitions folder' do
-      described_class.new
       Bumbleworks.definitions_directory.should == File.join(app_root, 'lib/process_definitions')
     end
 
     it 'discovers default Root/app/participants directory folder' do
-      described_class.new
       Bumbleworks.participants_directory.should == File.join(app_root, 'app', 'participants' )
     end
 
     it 'discovers default Root/participants directory folder' do
-      described_class.new
       Bumbleworks.root = File.join(app_root, 'app')
       Bumbleworks.participants_directory.should == File.join(app_root, 'app', 'participants' )
     end
 
-    it 'loads participants' do
+    it 'loads participants and adds the catchall at the end if not defined' do
+      Bumbleworks.root = File.join(app_root, 'app')
+      Bumbleworks.engine.participant_list.should have(3).items
+      Bumbleworks.engine.participant_list.map(&:classname).should =~ ['HoneyParticipant', 'MolassesParticipant', 'Ruote::StorageParticipant']
+    end
+
+    it 'loads participants and and does not add catchall' do
+      described_class.any_instance.stub(:register_participants) do
+        Bumbleworks.register_participants do
+          bees_honey 'BeesHoney'
+          maple_syrup 'MapleSyrup'
+          catchall 'NewCatchall'
+        end
+      end
+
       described_class.new
       Bumbleworks.root = File.join(app_root, 'app')
-      Bumbleworks.engine.participant_list.should have(2).items
-      Bumbleworks.engine.participant_list.map(&:classname).should =~ ['HoneyParticipant', 'MolassesParticipant']
+      Bumbleworks.engine.participant_list.should have(3).items
+      Bumbleworks.engine.participant_list.map(&:classname).should =~ ['BeesHoney', 'MapleSyrup', 'NewCatchall']
     end
 
     it 'loads process definitions' do
-      described_class.new
       Bumbleworks.root = File.join(app_root, 'app')
       Bumbleworks.engine.variables['make_honey'].should == ["define", {"name"=>"make_honey"}, [["dave", {"ref"=>"honey maker"}, []]]]
       Bumbleworks.engine.variables['garbage_collector'].should == ["define", {"name"=>"garbage_collector"}, [["george", {"ref"=>"garbage collector"}, []]]]
