bdb5e8c4e3eb607185e6c62fd1330ad992e9ff77
Maher Hawash <gmhawash@gmail.com>
2013-11-15 13:56:44 -0800

    Implement ErrorHandler class
    Logs error in process.

--- lib/bumbleworks/error_handler.rb
+++ lib/bumbleworks/error_handler.rb
@@ -1,7 +1,33 @@
 module Bumbleworks
   class ErrorHandler
-    def on_error
+    include WorkitemEntityStorage
 
+    attr_accessor :workitem
+    def initialize(workitem)
+      self.workitem = workitem
+    end
+
+    def on_error
+      return unless logger
+      logger.error(
+        :actor => workitem.wf_name,
+        :action => 'process error',
+        :target_type => entity_fields[:type],
+        :target_id => entity_fields[:identifier],
+        :metadata => metadata
+      )
+    end
+
+    private
+    def logger
+      Bumbleworks.logger
+    end
+
+    def metadata
+      {
+        :wfid => workitem.wfid,
+        :error => workitem.error,
+      }
     end
   end
 end
---
+++ spec/lib/bumbleworks/error_handler_spec.rb
@@ -0,0 +1,30 @@
+describe Bumbleworks::ErrorHandler do
+  subject {described_class.new(workitem)}
+  let(:workitem) {double(:wf_name => 'armadillo', :error => 'something is amiss in dillo land', :wfid => 'zabme123', :fields => {})}
+  it 'calls registered logger and logs error information' do
+    Bumbleworks.logger.should_receive(:error).with({
+      :actor => 'armadillo',
+      :action => 'process error',
+      :target_type => nil,
+      :target_id => nil,
+      :metadata => {:wfid => 'zabme123', :error => 'something is amiss in dillo land'}
+    })
+
+    subject.on_error
+  end
+
+  it 'sets target to entity if found' do
+    workitem.stub(:fields => {:entity_id => 1234, :entity_type => 'Lizards'})
+    Bumbleworks.logger.should_receive(:error).with(hash_including({
+      :target_type => 'Lizards',
+      :target_id => 1234,
+    }))
+
+    subject.on_error
+  end
+
+  it 'does nothing if logger is not registered' do
+    Bumbleworks.stub(:logger)
+    subject.on_error.should == nil
+  end
+end
